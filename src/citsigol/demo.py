070fbb68e5e7a6bd30123a5e6cb71708f7311015
7c-c7 <105086645+7c-c7@users.noreply.github.com>
2024-05-25 21:03:30 -0400

    cleanup demo.py

--- src/citsigol/demo.py
+++ src/citsigol/demo.py
@@ -13,11 +13,15 @@ def main() -> None:
     """Demo script for citsigol."""
     print("Welcome to citsigol!")
 
+    r_resolution = 24001
+    n_steps = 4000
+    n_skip = 400
+    r_limits = (2, 4)
     # plot the classic bifurcation diagram of the logistic map
     print("Plotting the classic bifurcation diagram of the logistic map...")
-    r_values = np.linspace(3.544, 4, 12001)
+    r_values = np.linspace(3.544, r_limits[1], r_resolution)
     x_values = logistic.bifurcation_diagram(
-        r_values, n_skip=100, n_steps=2000, track_progress=True, tol=1e-6
+        r_values, n_skip=n_skip, n_steps=n_steps, track_progress=True, tol=1e-6
     )
     plt.figure(figsize=(21, 11))
     plt.style.use("dark_background")
@@ -31,7 +35,7 @@ def main() -> None:
     print("\nDone!")
 
     # plot the stable limit cycles of the logistic map vs the parameter.
-    r_values = np.linspace(2, 4, 10001)
+    r_values = np.linspace(r_limits[0], r_limits[1], round(r_resolution / 2))
     max_period = 4
     x_values = [logistic.fixed_points(r, max_period) for r in r_values]
     """plt.figure(figsize=(21, 11))
@@ -40,12 +44,12 @@ def main() -> None:
     plt.ylabel("x")
     for x, r in zip(x_values, r_values):
         plt.plot(
-            r * np.ones_like(x), x, ".", markersize=0.3, alpha=0.4, color="aquamarine"
+            r * np.ones_like(x), x, ".", markersize=0.3, alpha=0.6, color="aquamarine"
         )
 
     # plot the citsigol map with a few different r and target values
     print("Plotting the citsigol map with a few different r and target values...")
-    n_iterations = 200
+    n_iterations = 50
     r_values = [0.5, 1.5, 2.5, math.pi, 3.5, 3.8]
     target_values = [0.25, 0.6, 1 / math.pi]
     exes = []
